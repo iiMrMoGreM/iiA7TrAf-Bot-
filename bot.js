a5858b93625334d4d30a4f51602db13c0f063126
iiMrMoGreM <40735508+iiMrMoGreM@users.noreply.github.com>
2018-07-25 23:20:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -446,4 +446,49 @@ client.on('guildMemberAdd', member => {
     channel.send({embed : embed});
     })
 
+
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "𝓲𝓲𝓐𝟕𝓣𝓻𝓐𝓯 𝓖𝓪𝓶𝓮𝓼")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', '💜𝓦𝓮𝓵𝓬𝓸𝓶𝓮💜');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "𝓲𝓲𝓐𝟕𝓣𝓻𝓐𝓯 𝓖𝓪𝓶𝓮𝓼")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
+
+
 client.login(process.env.BOT_TOKEN);
