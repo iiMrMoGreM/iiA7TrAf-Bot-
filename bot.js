5512df9ce72a99b4bbd87bc4b63e0ce1756a5dfb
iiMrMoGreM <40735508+iiMrMoGreM@users.noreply.github.com>
2018-07-25 04:04:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -331,4 +331,21 @@ client.on('message', function(msg) {
   });
 
 
+
+
+client.on('message', message => {
+
+  if (message.content.startsWith( prefix + "sug")) {
+  if (!message.channel.guild) return;
+  let args = message.content.split(" ").slice(1).join(' ');
+  client.channels.get("ايدي الروم").send(
+      "\n" + "**" + "● السيرفر :" + "**" +
+      "\n" + "**" + "» " + message.guild.name + "**" +
+      "\n" + "**" + " ● المرسل : " + "**" +
+      "\n" + "**" + "» " + message.author.tag + "**" +
+      "\n" + "**" + " ● الرسالة : " + "**" +
+      "\n" + "**" + args + "**")
+  }
+  });
+
 client.login(process.env.BOT_TOKEN);
