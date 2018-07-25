eed2420ec511b085d1d8b4f32f60afedd92781e8
iiMrMoGreM <40735508+iiMrMoGreM@users.noreply.github.com>
2018-07-25 03:59:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -307,33 +307,6 @@ message.channel.send({ embed  : EmojiEmbed });
 
 
 
-```js
-client.on('message', message => {
-   if (message.content.startsWith("=idd")) {
-                if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-               var mentionned = message.mentions.users.first();
-    var mentionavatar;
-      if(mentionned){
-          var mentionavatar = mentionned;
-      } else {
-          var mentionavatar = message.author;
-          
-      }
-   let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-   
-  .addField("Name:",`<@` + `${mentionavatar.id}` + `>`, true)
-  .addField('Discrim:',"#" +  `${mentionavatar.discriminator}`, true)
-   .addField("ID:", "**[" + `${mentionavatar.id}` + "]**", true)
-  .addField("Create At:", "**[" + `${mentionavatar.createdAt}` + "]**", true)
-     
-     
-  message.channel.sendEmbed(embed);
-  console.log('[id] Send By: ' + message.author.username)
-    }
-});
-
 
 
 
