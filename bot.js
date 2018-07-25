19e749956ebd745e6db3a089bd6c9fd3ca856475
iiMrMoGreM <40735508+iiMrMoGreM@users.noreply.github.com>
2018-07-25 07:28:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -375,42 +375,5 @@ client.on('message', message => {
 
 
 
-var kboosh_id = "471548405626306560";
-                    var i = "0";
-                    var x = "0";
-
-    x++;
-    kboosh.editChannel(kboosh_id, { name : "Voice ⇏「" + x + "」"});
-});
-kboosh.on("voiceChannelLeave", (msg) => {
-    x--;
-    kboosh.editChannel(kboosh_id, { name : "Voice ⇏「" + x + "」"});
-});
-
-kboosh.on("messageCreate", (msg) => {
-    if(msg.author.id !== "228190640939335682") return kboosh.createMessage('__**This Command is only for the bot Owner**__');
-    if(msg.content === "$voice") {
-        let users = msg.channel.guild.members.map(m => m.user.id);
-        let messages = [];
-        messages.push(users);
-        setTimeout(function(){
-        while (i <= messages[0].length - 1) {
-            check = msg.channel.guild.members.get(messages[0][i]);
-        if(!check.voiceState.channelID){
-                i++;
-        }else{
-                x++;
-                i++;
-        }
-}
-    console.log(x);
-    kboosh.createMessage(msg.channel.id, "Voice Online Members Now Are: **"+x+"** Members!");
-    kboosh.editChannel(kboosh_id, { name : "Voice ⇏「"+x+"」"});
-    messages = [];
-}, 1);
-    }
-});
-
-
 
 client.login(process.env.BOT_TOKEN);
