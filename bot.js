befe653a1dbe51b24158aeaf35be8d5d7cb3b50d
iiMrMoGreM <40735508+iiMrMoGreM@users.noreply.github.com>
2018-07-25 23:32:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -491,4 +491,24 @@ client.on("guildMemberAdd", (member) => {
 });
 
 
+
+client.on("message", message => {
+
+              
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === prefix + "image"){ 
+          const embed = new Discord.RichEmbed()
+  
+      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+
+   message.channel.send({embed});
+      }
+  });
+
 client.login(process.env.BOT_TOKEN);
